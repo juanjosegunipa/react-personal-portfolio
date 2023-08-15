a6290bd76762e99a97e4f3fb9034ae6931f0e742
juanjguanipaf@gmail.com <juanjguanipaf@gmail.com>
2023-08-15 08:56:18 -0400

    2nd commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './about.css';
+
+const About = () => {
+    return (
+        <>
+            <div>About</div>
+            <h1>Hello</h1>
+        </>
+
+    )
+}
+
+export default About
